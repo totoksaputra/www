4c26e82c856f2e77ab5d9cf543121a74daa564f4
totoksaputra <capekpeang@gmail.com>
2015-09-24 23:51:17 +0700

    Create home.php

---
+++ page/home.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<ul data-role="listview" data-filter="true" data-filter-placeholder="Search fruits..." data-input="#autocomplete-input" data-inset="true">
+<li><a href="index.php?mod=page&pg=data_mp3&id=001AlFatihaah.mp3">Al fatihah</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=002AlBaqoroh.mp3">Al Baqoroh</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=003AliImropn.mp3">Ali 'Imroon</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=004AnNisaa.mp3">An Nisaa'</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=005AlMaaidah.mp3">Al Maa'idah</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=006AlAnaam.mp3">Al An'aam</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=007AlAraaf">Al A'raaf</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=008AlAnfaal.mp3">Al Anfaal</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=009AtTaubah.mp3">At Taubah</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=010Yuunus.mp3">Yuunus</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=011Huud.mp3">Huud</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=012Yuusuf.mp3">Yuusuf</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=013ArRadu.mp3">Ar Ra'du</a></li>
+<li><a href="index.php?mod=page&pg=data_mp3&id=014Ibrahim.mp3">Ibrahim</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=015AlHijr.mp3">Al Hijr</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=016AnNahl.mp3">An Nahl</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=017AlIsraa.mp3">Al Israa'</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=018AlKahfi.mp3">Al Kahfi</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=019Maryam.mp3">Maryam</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=020Thooha.mp3">Thooha</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=021AlAnbiyaa.mp3">Al Anbiyaa</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=022AlHaj.mp3">Al Haj</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=023AlMuminuun.mp3">Al Muminuun</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=024AnNuur.mp3">An Nuur</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=025AlFurqoon.mp3">Al Furqoon</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=026AsySyuaro.mp3">Asy Syu'aro</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=027AnNaml.mp3">An Naml</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=028AlQoshosh.mp3">Al Qoshosh</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=029AlAnkabuut.mp3">Al Ankabuut</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=030ArRuum.mp3">Ar Ruum</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=031Luqmaan.mp3">Luqmaan</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=032AsSajadah.mp3">As Sajadah</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=033AlAhzaab.mp3">Al Ahzaab</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=034Saba.mp3">Saba</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=035Faathir.mp3">5Faathir</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=036Yaasiin.mp3">Yaasiin</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=037Ash Shooffaat.mp3">Ash Shooffaat</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=038Shood.mp3">Shood</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=039AzZumar.mp3">Az Zumar</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=040Ghoofir.mp3">Ghoofir</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=041Fushshilat.mp3">Fushshilat</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=042AsySyuro.mp3">Asy Syuro</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=043AzZukhruf.mp3">Az Zukhruf</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=044AdDuhaan.mp3">Ad Duhaan</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=045AlJaatsiyah.mp3">Al Jaatsiyah</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=046AlAhqaf.mp3">Al Ahqaf</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=047Muhammad.mp3">Muhammad</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=048AlFath.mp3">Al Fath</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=049AlHujurot.mp3">Al Hujurot</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=050Qoof.mp3">Qoof</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=051AdzDzaariyat.mp3">Adz Dzaariyat</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=052Thuur.mp3">Thuur</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=053AnNajm.mp3">An Najm</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=054AlQomar.mp3">Al Qomar</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=055ArRohmaan.mp3">Ar Rohmaan</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=056AlWaaqiah.mp3">Al Waaqi'ah</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=057AlHadiid.mp3">Al Hadiid</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=058AlMujaadilah.mp3">Al Mujaadilah</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=059AlHasyr.mp3">Al Hasyr</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=060AlMumtahanah.mp3">Al Mumtahanah</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=061AshShaff.mp3">Ash Shaff</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=062AlJumuah.mp3">Al Jumu'ah</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=063AlMunafiqun.mp3">Al Munafiqun</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=064AtTaghabun.mp3">At Taghabun</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=065AtTalaq.mp3">At Talaq</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=066AtTahrim.mp3">At Tahrim</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=067AlMulk.mp3">Al Mulk</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=068AlQalam.mp3">Al Qalam</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=069AlHaqqah.mp3">Al Haqqah</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=070AlMaarij.mp3">Al Ma'arij</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=071Nuh.mp3">Nuh</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=072AlJin.mp3">Al Jin</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=073AlMuzzammil.mp3">Al Muzzammil</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=074AlMuddatsir.mp3">Al Muddatsir</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=075AlQiyamah.mp3">Al Qiyamah</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=076AlInsaan.mp3">Al Insaan</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=077AlMursalat.mp3">Al Mursalat</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=078AnNaba.mp3">An Naba</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=079AnNaaziaat.mp3">An Naaziaat</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=080Abasa.mp3">Abasa</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=081AtTakwiir.mp3">At Takwiir</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=082AlInfithoor.mp3">Al Infithoor</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=083 Al Muthoffifiin.mp3">Al Muthoffifiin</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=084 Al Insyiqooq.mp3">Al Insyiqooq</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=085 Al Buruuj.mp3">Al Buruuj</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=086 Ath Thooriq.mp3">Ath Thooriq</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=087 Al A'laa.mp3">Al A'laa</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=088 Al Ghoosyiyah.mp3">Al Ghoosyiyah</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=089 Al Fajr.mp3">Al Fajr</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=090 Al Balad.mp3">Al Balad</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=091 Asy Syams.mp3">Asy Syams</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=092 Al Lail.mp3">Al Lail</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=093 Adh Dhuha.mp3">Adh Dhuha</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=094 An Nasyroh.mp3">An Nasyroh</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=095 At Tiin.mp3">At Tiin</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=096 Al 'Alaq.mp3">Al 'Alaq</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=097 Al Qodr.mp3">Al Qodr</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=098 Al Bayyinah.mp3">Al Bayyinah</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=099 Al Zalzalah.mp3">Al Zalzalah</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=100 Al 'Aadiyaat.mp3">Al 'Aadiyaat</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=101 Al Qoori'ah.mp3">Al Qoori'ah</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=102 At Takaatsur.mp3">At Takaatsur</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=103 Al 'Ashr.mp3">Al 'Ashr</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=104 Al Humazah.mp3">Al Humazah</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=105 Al Fiil.mp3">Al Fiil</a></li>
+	 <li><a href="index.php?mod=page&pg=data_mp3&id=106 Quroisy.mp3">Quroisy</a></li>
+	  <li><a href="index.php?mod=page&pg=data_mp3&id=107 Al Maa'uun.mp3">Al Maa'uun</a></li>
+	   <li><a href="index.php?mod=page&pg=data_mp3&id=108 Al Kautsar.mp3">Al Kautsar</a></li>
+	    <li><a href="index.php?mod=page&pg=data_mp3&id=109 Al Kaafiruun.mp3">Al Kaafiruun</a></li>
+		 <li><a href="index.php?mod=page&pg=data_mp3&id=110 An Nashr.mp3">An Nashr</a></li>
+		  <li><a href="index.php?mod=page&pg=data_mp3&id=111 Al Masad.mp3">Al Masad</a></li>
+		   <li><a href="index.php?mod=page&pg=data_mp3&id=112 Al Ikhlas.mp3">Al Ikhlas</a></li>
+		    <li><a href="index.php?mod=page&pg=data_mp3&id=113 Al Falaq.mp3">Al Falaq</a></li>
+			 <li><a href="index.php?mod=page&pg=data_mp3&id=114 An Naas.mp3">An Naas</a></li>
+</ul>
+</body>
+</html>
